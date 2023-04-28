bf8b50b41b746ed28b495a6ae0dc7bf7d8660f51
surya2dot0 <suryap1802@gmail.com>
2023-04-28 17:50:01 +0530

    First Commit

---
+++ SpringSecurityRoles/src/main/java/in/learning/SpringSecurityRolesApplication.java
@@ -0,0 +1,13 @@
+package in.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityRolesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityRolesApplication.class, args);
+	}
+
+}
